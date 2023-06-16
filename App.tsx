d4f893480abe4f78079570dd3d0c9491b53992b3
goku-ts <hgb.mega@gmail.com>
2023-06-16 16:05:19 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import { Notes } from './screens/Notes/Notes';
+import SignupScreen from './screens/User/Register';
+import SignInScreen from './screens/User/Login';
+import PostForm from './screens/Notes/Edits';
+import Navigator from './navigation/Navigator';
 
-export default function App() {
+const App=()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style="light" />  
+    <Navigator/>
+    </>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
